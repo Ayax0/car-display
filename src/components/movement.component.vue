dcda48436aa4b41d19993003d86929e0b8f0f4bb
Simon Gander <simon_gander@nextlvlup.com>
2022-10-01 17:13:04 +0200

    update gps frequency

--- src/components/movement.component.vue
+++ src/components/movement.component.vue
@@ -1,6 +1,4 @@
 <script>
-import { computeDestinationPoint, getCenterOfBounds, getDistance, getRhumbLineBearing } from "geolib";
-
 import Route from "@components/route.component.vue";
 
 export default {
@@ -18,7 +16,6 @@ export default {
 	data() {
 		return {
 			interval: undefined,
-			payload: [],
 			fixedPosition: undefined,
 			fixedDirection: undefined,
 			path: undefined,
@@ -38,7 +35,7 @@ export default {
 		position: {
 			deep: true,
 			handler(value) {
-				this.payload.push(value);
+				this.move(value);
 			},
 		},
 		fixedPosition: {
@@ -50,35 +47,6 @@ export default {
 	},
 	mounted() {
 		this.map.panTo(this.position);
-		this.interval = setInterval(() => {
-			if (this.ready && this.payload.length > 0) {
-				const _payload = [];
-				while (this.payload.length > 0) _payload.push(this.payload.shift());
-				let centerPosition = getCenterOfBounds(_payload.map((item) => ({ latitude: item.lat, longitude: item.lng })));
-				let averageDirection =
-					_payload
-						.filter((item) => item.track != undefined)
-						.map((item) => item.track)
-						.reduce((value, current) => value + current) / _payload.filter((item) => item.track != undefined).length;
-
-				// if (this.path) {
-				// 	const startDistance = getDistance(parsePosition(this.path.from), centerPosition, 0.1);
-				// 	const pathDirection = getRhumbLineBearing(parsePosition(this.path.from), parsePosition(this.path.to));
-				// 	const predictedPosition = computeDestinationPoint(parsePosition(this.path.from), startDistance, pathDirection);
-				// 	console.log(
-				// 		getDistance(centerPosition, predictedPosition),
-				// 		getDistanceFromLine(centerPosition, parsePosition(this.path.from), parsePosition(this.path.to), 0.1)
-				// 	);
-				// 	if (getDistanceFromLine(centerPosition, parsePosition(this.path.from), parsePosition(this.path.to), 0.1) < 50)
-				// 		centerPosition = predictedPosition;
-				// }
-
-				this.move(this.fixedPosition, this.fixedDirection, { lat: centerPosition.latitude, lng: centerPosition.longitude }, averageDirection);
-
-				this.fixedPosition = { lat: centerPosition.latitude, lng: centerPosition.longitude };
-				this.fixedDirection = averageDirection;
-			}
-		}, 2000);
 	},
 	beforeUnmount() {
 		clearInterval(this.interval);
@@ -104,28 +72,11 @@ export default {
 				}
 			);
 		},
-		async move(from, fromHeading, to, toHeading) {
+		async move(to) {
 			if (!this.map) return;
-			if (!from && !to) return;
-			if (!from) return this.map.panTo(to);
-			if (!fromHeading) return this.map.setHeading(toHeading);
-			const stepAmount = 30;
-			const distance = getDistance(from, to, 0.01);
-			let headingDistance = toHeading - fromHeading;
-			// if(headingDistance >= 180 || headingDistance <= -180) {
+			if (!to) return;
 
-			// }
-
-			const step = distance / stepAmount;
-			const headingStep = headingDistance / stepAmount;
-			const bearing = getRhumbLineBearing(from, to);
-			for (let i = 0; i < stepAmount; i++) {
-				const newPosition = computeDestinationPoint(from, step * (i + 1), bearing);
-				this.map.panTo({ lat: newPosition.latitude, lng: newPosition.longitude });
-				this.map.setHeading(fromHeading + (i + 1) * headingStep);
-				await new Promise((r) => setTimeout(r, 2000 / stepAmount));
-			}
-			this.map.panTo(to), this.map.setHeading(toHeading);
+			this.map.panTo(to), this.map.setHeading(to.track);
 			if (this.position.speed > 70) this.map.setZoom(16);
 			else this.map.setZoom(18);
 		},
